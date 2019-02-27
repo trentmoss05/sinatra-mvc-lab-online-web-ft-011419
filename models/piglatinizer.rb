9d6a8350965501dfefbce98feb7fc4f994b00774
Trent Moss <trentmoss05@gmail.com>
2019-02-27 20:28:08 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,16 +7,20 @@ class PigLatinizer
   private
 
   def consonant?(char)
-    !char.match(/aAeEiIoOuU/)
+    !char.match(/[aAeEiIoOuU]/)
   end
 
   def piglatinize_word(word)
+    # word starts with vowel
     if !consonant?(word[0])
       word = word + "w"
-    elsif consonant?(word[0]) && consonant(word[1]) && consonant?(word[2])
-      word = word.slice(3..1) + word.slice(0,3)
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
     elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
     else
       word = word.slice(1..-1) + word.slice(0)
     end
