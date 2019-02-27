0beb34fc4a040c60fdad1cf37af58ac75e2e368e
Trent Moss <trentmoss05@gmail.com>
2019-02-27 20:23:42 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
 
   def piglatinize(input_str)
-    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_word(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
 
   private
