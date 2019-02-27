8643515c6d8cc5d550afaaa7ffdc90a486b8a2e7
Trent Moss <trentmoss05@gmail.com>
2019-02-27 20:23:31 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,4 +10,17 @@ class PigLatinizer
     !char.match(/aAeEiIoOuU/)
   end
 
+  def piglatinize_word(word)
+    if !consonant?(word[0])
+      word = word + "w"
+    elsif consonant?(word[0]) && consonant(word[1]) && consonant?(word[2])
+      word = word.slice(3..1) + word.slice(0,3)
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
 end
