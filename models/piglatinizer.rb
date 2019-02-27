0ae05ae69e899bf180cb80935e4bddd689e7fddd
Trent Moss <trentmoss05@gmail.com>
2019-02-27 20:20:38 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,6 +2,12 @@ class PigLatinizer
 
   def piglatinize(input_str)
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_word(input_str)
+  end
 
+  private
+
+  def consonant?(char)
+    !char.match(/aAeEiIoOuU/)
+  end
 
 end
