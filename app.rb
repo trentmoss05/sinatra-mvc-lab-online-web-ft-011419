3e0a310053472d655303bb603c57d2fb9d9ad451
Trent Moss <trentmoss05@gmail.com>
2019-02-27 20:25:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     pl = PigLatinizer.new
-    @piglatin = pl.piglatinize(params[:user_phrase])
+    @piglatin = pl.piglatinize(params[:user_input])
     erb :results
   end
 end
